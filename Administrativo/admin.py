0231130dfe6c3215e8f98542acbb939169b6f0b0
lvsdvale <vale@alunos.utfpr.edu.br>
2020-09-20 01:28:25 -0300

    Modelos financeiros

--- Administrativo/admin.py
+++ Administrativo/admin.py
@@ -5,7 +5,23 @@ from .models import *
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ['name']
     search_fields = ['name']
+
 @admin.register(documento)
 class DocumentoAdmin(admin.ModelAdmin):
     list_display = ['name','categoria','pub_date']
     search_fields = ['name','categoria']
+
+@admin.register(custo)
+class CustoAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(contaspagar)
+class PagarAdmin(admin.ModelAdmin):
+    list_display = ['name','Custo','pub_date']
+    search_fields = ['name','Custo','fornecedor']
+
+@admin.register(contasreceber)
+class ReceberAdmin(admin.ModelAdmin):
+    list_display = ['name','Custo','pub_date']
+    search_fields = ['name','Custo']
--- Administrativo/models.py
+++ Administrativo/models.py
@@ -17,3 +17,40 @@ class documento(models.Model):
         verbose_name = 'Documento'
         verbose_name_plural = 'Documentos'
         ordering = ['name']
+
+class custo(models.Model):
+    name = models.CharField(max_length=250,null=False,blank=False,verbose_name="Nome")
+    class Meta:
+        verbose_name = 'Centro de custo'
+        verbose_name_plural = 'Centros de custo'
+        ordering = ['name']
+
+class contaspagar(models.Model):
+    name = models.CharField(max_length=250, null=False, blank=False, verbose_name="Título")
+    data = models.DateField(auto_now_add=True,verbose_name="Data de Vencimento")
+    fornecedor = models.CharField(max_length=255,null=False,blank=False,verbose_name="Fornecedor")
+    valor = models.DecimalField(max_digits=10,decimal_places=2,verbose_name="Valor do título")
+    juros = models.DecimalField(max_digits=10,decimal_places=2,verbose_name="Juros")
+    barras = models.IntegerField(verbose_name="Código de Barras")
+    Custo = models.ForeignKey(custo, verbose_name='Centro de custo',null=True,
+                               related_name='Pagar',on_delete=models.CASCADE)
+    arquivo = models.FileField(upload_to=get_file_path,null=False,blank=True,verbose_name="Nota")
+    pub_date = models.DateField(auto_now_add=True, verbose_name='Data de Criação')
+    class Meta:
+        verbose_name = 'Conta a pagar'
+        verbose_name_plural = 'Contas a pagar'
+        ordering = ['name']
+
+class contasreceber(models.Model):
+    name = models.CharField(max_length=250, null=False, blank=False, verbose_name="Título")
+    data = models.DateField(auto_now_add=True,verbose_name="Data do recebimento")
+    valor = models.DecimalField(max_digits=10,decimal_places=2,verbose_name="Valor do título")
+    juros = models.DecimalField(max_digits=10,decimal_places=2,verbose_name="Juros")
+    Custo = models.ForeignKey(custo, verbose_name='Centro de custo',null=True,
+                               related_name='Receber',on_delete=models.CASCADE)
+    arquivo = models.FileField(upload_to=get_file_path,null=False,blank=True,verbose_name="Nota")
+    pub_date = models.DateField(auto_now_add=True, verbose_name='Data de Criação')
+    class Meta:
+        verbose_name = 'Conta a receber'
+        verbose_name_plural = 'Contas a receber'
+        ordering = ['name']
