6496d8f55c993561697fdbcc3837ecd532f745fe
lvsdvale <vale@alunos.utfpr.edu.br>
2021-02-03 23:13:38 -0300

    banner dots

--- Avalancheutfpr/static/js/banner.js
+++ Avalancheutfpr/static/js/banner.js
@@ -1,18 +1,26 @@
  var _index = 0;
-    showSlide();
-    function showSlide(){
+ var timer;
+    showSlide(_index);
+
+    function set_slide(n){
+        showSlide(_index = n)
+    };
+
+    function showSlide(n){
+
     var $slide = document.querySelectorAll(".banner .slide")
     var $pointer = document.querySelectorAll(".banner .sliders-pointers div")
     var $tam_slide = $slide.length
+
         for(var i=0;i<$tam_slide;i++){
              $slide[i].style.display= "none";
         }
-        _index++;
-        if (_index > $tam_slide) {_index = 1}
         for (i = 0; i < $pointer.length; i++) {
         $pointer[i].className = $pointer[i].className.replace(" active", "");
         }
-        $slide[_index-1].style.display = "block";
-        $pointer[_index-1].className += " active";
-        setTimeout(showSlide, 7000);
+        if (_index >= $tam_slide) {_index = 0}
+        $slide[_index].style.display = "block";
+        $pointer[_index].className += " active";
+        clearTimeout(timer);
+        timer = setTimeout(() => set_slide(_index+1), 5000);
     }
