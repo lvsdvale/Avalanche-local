6e9781821285b31635868c8b89c528574eb3d285
lvsdvale <vale@alunos.utfpr.edu.br>
2021-03-02 23:25:22 -0300

    cargos

--- Avalancheutfpr/services.py
+++ Avalancheutfpr/services.py
@@ -3,6 +3,7 @@ import smtplib
 from email.message import EmailMessage
 from django.conf import settings
 import ssl
+from Contas.models import user
 
 def get_file_path(_instance,filename):
     ext = filename.split(',')[-1]
@@ -20,7 +21,7 @@ def Send_Esportes_Mail(usuario,modalidade):
         server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
         msg = EmailMessage()
         subject = f'Inscrição {modalidade.name} avalanche'
-        body = f'Seja bem vindx aos esportes do ursão !!! \n{usuario.name}, recebemos sua inscrição logo entraremos em contato \nqualquer dúvida entrar em contato com a direção da A.A.A.E.A UTFPR-CT.'
+        body = f'Eaii, {usuario.name}\n Sua inscrição foi confirmada, seja bem vindo ao time de {modalidade.name}. Mal vejo a hora de ver sua garra vestindo a nossa camisa, qualquer dúvida entre em contato com o coordenador de {modalidade.name}\nTe espero nos treinos!\nAbaços\nPaul Lar.'
         to = f'{usuario.email}'
         msg.set_content(body)
         msg['Subject'] = subject
@@ -70,7 +71,7 @@ def Send_Acao_Mail(usuario,campanha):
         server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
         msg = EmailMessage()
         subject=f'Inscrição na ação Social:{campanha.name}'
-        body=f'{usuario.name}, recebemos sua inscrição na ação {campanha.name} logo entraremos em contato para juntos fazermos o bem ! \nqualquer dúvida entrar em contato com a direção da A.A.A.E.A UTFPR-CT'
+        body=f'Olá {usuario.name}\nQue alegria ver você inscrito na ação {campanha.name}. Logo o time de social entrará em contato, pode ir preparando o coração!\nCarinhosamente,\nPaul Lar'
         to = f'{usuario.email}'
         msg.set_content(body)
         msg['Subject'] = subject
@@ -93,7 +94,7 @@ def Send_Sign_Mail(Email,Nome):
         server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
         msg = EmailMessage()
         subject=f'Cadastro Concluido'
-        body=f'Seja muito bem vindo a Familia Avalanche!!! \n{Nome}, Seu cadastro foi concluído com sucesso, agora você pode aproveitar todo o nosso site.'
+        body=f'Olaaaar {Nome}, seja bem vindo à plataforma oficial da AVALANCHE, \nAqui você vai poder se inscrever nas modalidades, adquirir produtos avalanche,  acompanhar informações sobre os eventos, acessar conteúdos exclusivos e muito mais!\nP.S. Fique ligado no nosso instagram para receber as atualizações do site\nCarinhosamente,\nPaul Lar'
         to = f'{Email}'
         msg.set_content(body)
         msg['Subject'] = subject
@@ -110,6 +111,7 @@ def Send_Sign_Mail(Email,Nome):
 def Send_Reset_Mail(Email,Senha):
     context = ssl.create_default_context()
     server = None
+    usuario = user.objects.get(email=Email)
     try:
         server = smtplib.SMTP("smtp.gmail.com", settings.EMAIL_PORT)
         server.ehlo()  # Can be omitted
@@ -118,7 +120,7 @@ def Send_Reset_Mail(Email,Senha):
         server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
         msg = EmailMessage()
         subject=f'Recuperação de senha'
-        body=f'A sua nova senha é {Senha}'
+        body=f'Fala tu,{usuario.name}\nEsquecer a senha até vale, só não vale esquecer de participar do parceladão!\nP.S. Tá ai sua cerveja, ops senha:{Senha}\nCarinhosamente,\nPaul Lar'
         to = f'{Email}'
         msg.set_content(body)
         msg['Subject'] = subject
